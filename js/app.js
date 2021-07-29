45719f130d8cfe992ef79652343685216c6397a6
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-07-28 20:49:50 -0500

    create primary variables

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listarCursos = document.querySelector('#lista-cursos');
+
