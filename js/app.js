157d0da6035afa609b7d1b0274511925b3e44894
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-07-28 21:00:08 -0500

    function that triggers the add to cart event

--- js/app.js
+++ js/app.js
@@ -3,7 +3,16 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
-cargarEventListener();
-function cargarEventListener() {
+cargarEventListeners();
+function cargarEventListeners() {
+    // Cuando agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
 
+// Funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        console.log(e.target);
+    }
 }
