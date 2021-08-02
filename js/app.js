26aa79568d66313da2b57ae6b7d910397313fb3a
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-08-02 08:46:48 -0500

    add cart to tbody

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -13,27 +14,49 @@ function cargarEventListeners() {
 // Funciones
 function agregarCurso(e){
     e.preventDefault();
-
+    // Delegation para agregar-carrito
     if(e.target.classList.contains('agregar-carrito')) {
-        const cursoSeleccionado = e.target.parentElement.parentElement;
-        leerDatosCurso(cursoSeleccionado);
+        const curso = e.target.parentElement.parentElement;
+        // Enviamos el curso seleccionado para tomar sus datos
+        leerDatosCurso(curso);
     }
 
 }
 
 // Lee el contenido del HTML al que le dimos  click y extrae la informacion del curso
 function leerDatosCurso(curso) {
-    console.log(curso);
+    // console.log(curso);
 
     // Crear objeto con el contenido del curso actual
-
     const infoCurso = {
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('.precio span').textContent,
-        id: curso.querySelector('a').getAttribute('data-id'),
-        cantidad: 1,
-    }
+        id: curso.querySelector('a').getAttribute('data-id'), 
+        cantidad: 1
+   }
+    
+    // Agrega elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
 
-    console.log(infoCurso);
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+    
+    articulosCarrito.forEach(curso => {
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+        
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
 }
