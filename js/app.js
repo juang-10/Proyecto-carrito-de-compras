f8150a46cc44d0755cd0f17e8d72d86f1a00d6e2
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-08-02 08:52:14 -0500

    clear the first element of the HTML

--- js/app.js
+++ js/app.js
@@ -46,17 +46,36 @@ function leerDatosCurso(curso) {
 
 // Muestra el carrito de compras en el HTML
 function carritoHTML() {
+     
+    // Limpiar el HTML
+     limpiarHTML();
     
     articulosCarrito.forEach(curso => {
-
+        const  { imagen, titulo, precio, cantidad, id  } = curso;
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>
-                ${curso.titulo}
+                <img src="${imagen}" width="100">
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
             </td>
         `;
         
         // Agrega el HTML del carrito en el tbody
         contenedorCarrito.appendChild(row);
     });
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
